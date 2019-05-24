d911a9b8c1a5a943085010e765530c58399e69af
Wesseb <wesseb95@gmail.com>
2019-05-24 22:45:37 +0200

    Dodanie podstrony oferty kolczyków - TODO: Podstrona złotego kolczyka

--- JS/Scrollowanie.js
+++ JS/Scrollowanie.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $(".navbar a[href='#strona']").on('click', function(event) {
+$(document).ready(function () {
+    $(".navbar a[href='#strona']").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
 
@@ -7,9 +7,9 @@ $(document).ready(function(){
 
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 900, function(){
+            }, 900, function () {
                 window.location.hash = hash;
             });
         }
     });
-})
+});
